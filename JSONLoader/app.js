1a2cbb451e85dae9c4dfe9aac7e487787e24aff2
Jaka Bernard <jb6407@student.uni-lj.si>
2017-11-23 07:30:58 +0000

    malo igranja

--- JSONLoader/app.js
+++ JSONLoader/app.js
@@ -1,6 +1,46 @@
+var canvas;
 var gl;
 
+var vertexShaderText;
+var fragmentShaderText;
+
+var vertexShader;
+var fragmentShader;
+
+var shaderProgram;
+
+var sphereModel;
+var sphereTexture;
+
+
+var modelArray = [];
+var modelTextureIndexArray = [];
+var bufferArray = [];
+var textureArray = [];
+
+var numberOfModels = 1;
+var modelsLoaded = 0;
+
+var numberOfTextures = 1;
+var texturesLoaded = 0;
+
+var matWorldUniformLocation;
+var matViewUniformLocation;
+var matProjUniformLocation;
+
+var worldMatrix;
+var viewMatrix;
+var projMatrix;
+var xRotationMatrix;
+var yRotationMatrix;
+
+var lastTime = 0;
+
+
+
+
 var InitDemo = function () {
+	/*
 	loadTextResource('./shader.vs.glsl', function (vsErr, vsText) {
 		if (vsErr) {
 			alert('Fatal error getting vertex shader (see console)');
@@ -14,7 +54,7 @@ var InitDemo = function () {
 					loadJSONResource('./sferaTestna.json', function (modelErr, modelObj) {
 						if (modelErr) {
 							alert('Fatal error getting Susan model (see console)');
-							console.error(fsErr);
+							console.error(modelErr);
 						} else {
 							loadImage('./newTestBarva.png', function (imgErr, img) {
 								if (imgErr) {
@@ -30,12 +70,279 @@ var InitDemo = function () {
 			});
 		}
 	});
+	
+	//nalozimo vertex shader
+	loadTextResource('./shader.vs.glsl', function(vsErr, vsText) {
+		if (vsErr) {
+			alert('Fatal error getting vertex shader (see console)');
+			console.error(vsErr);
+		} else {
+			vertexShaderText = vsText
+		}
+	});
+	
+	//nalozimo fragment shader
+	loadTextResource('./shader.fs.glsl', function(fsErr, fsText) {
+		if (fsErr) {
+			alert('Fatal error getting fragment shader (see console)');
+					console.error(fsErr);
+		} else {
+			fragmentShaderText = fsText
+		}
+	});
+	
+	//nalozimo model krogle
+	loadJSONResource('./sferaTestna.json', function (modelErr, modelObj) {
+		if (modelErr) {
+			alert('Fatal error getting Susan model (see console)');
+			console.error(modelErr);
+		} else {
+			sphereModel = modelObj;
+		}
+	});
+	//nalozimo texturo krogle
+	loadImage('./newTestBarva.png', function (imgErr, img) {
+		if (imgErr) {
+			alert('Fatal error getting Susan texture (see console)');
+			console.error(imgErr);
+		} else { 
+			sphereTexture = img;
+		}
+	});
+	*/
+	initShaderText();
+	modelLoad('./sferaTestna.json', 0, 0);
+	textureLoad('./newTestBarva.png', 0);
+	
+	init();
+	
+	if (gl) {
+	    gl.clearColor(0.0, 0.0, 0.0, 1.0);                      // Set clear color to black, fully opaque
+	    gl.clearDepth(1.0);                                     // Clear everything
+	    gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
+	    gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
+	
+	    
+	    // Set up to draw the scene periodically.
+	    setInterval(function() {
+	      if (texturesLoaded == numberOfTextures && numberOfModels == modelsLoaded) { // only draw scene and animate when textures are loaded.
+	        requestAnimationFrame(animate);
+	        drawScene();
+	      }
+	    }, 15);
+	  }
+	
 };
 
+function animate() {
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+
+    // rotate the moon and the crate for a small amount
+    //moonAngle += 0.05 * elapsed;
+    //cubeAngle += 0.05 * elapsed;
+  }
+  lastTime = timeNow;
+}
+
+
+function drawScene() {
+	
+}
+////////////////////////////////////////za dodajanje modelov v tabelo
+function modelLoad(location, index, texIndex) {
+	loadJSONResource(location, function (modelErr, modelObj) {
+		if (modelErr) {
+			alert('Fatal error getting Susan model (see console)');
+			console.error(modelErr);
+		} else {
+			modelArray[index] = modelObj;
+			modelTextureIndexArray[index] = texIndex;
+			modelsLoaded++;
+		}
+	});
+}
+////////////////////////////////////////za dodajanje textur v tabelo
+function textureLoad(location, index) {
+	loadImage(location, function (imgErr, img) {
+		if (imgErr) {
+			alert('Fatal error getting Susan texture (see console)');
+			console.error(imgErr);
+		} else { 
+			textureArray[index] = img;
+			texturesLoaded++;
+		}
+	});
+}
+//////////////////////////////////////////////////////////inicializacija shader texta
+function initShaderText() {
+	//nalozimo vertex shader
+	loadTextResource('./shader.vs.glsl', function(vsErr, vsText) {
+		if (vsErr) {
+			alert('Fatal error getting vertex shader (see console)');
+			console.error(vsErr);
+		} else {
+			vertexShaderText = vsText
+		}
+	});
+	
+	//nalozimo fragment shader
+	loadTextResource('./shader.fs.glsl', function(fsErr, fsText) {
+		if (fsErr) {
+			alert('Fatal error getting fragment shader (see console)');
+					console.error(fsErr);
+		} else {
+			fragmentShaderText = fsText
+		}
+	});
+}
+
+function init() {
+	canvas = document.getElementById('game-surface');
+	gl = canvas.getContext('webgl');
+	
+	if (!gl) {
+		console.log('WebGL not supported, falling back on experimental-webgl');
+		gl = canvas.getContext('experimental-webgl');
+	}
+
+	if (!gl) {
+		alert('Your browser does not support WebGL');
+	}
+	
+	gl.clearColor(0.75, 0.85, 0.8, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.enable(gl.DEPTH_TEST);
+	gl.enable(gl.CULL_FACE);
+	gl.frontFace(gl.CCW);
+	gl.cullFace(gl.BACK);
+	
+	//
+	// Create shaders
+	// 
+	vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+	gl.shaderSource(vertexShader, vertexShaderText);
+	gl.shaderSource(fragmentShader, fragmentShaderText);
+
+	gl.compileShader(vertexShader);
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+		return;
+	}
+
+	gl.compileShader(fragmentShader);
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+		return;
+	}
+	
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+		console.error('ERROR linking program!', gl.getProgramInfoLog(shaderProgram));
+		return;
+	}
+	gl.validateProgram(shaderProgram);
+	if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
+		console.error('ERROR validating program!', gl.getProgramInfoLog(shaderProgram));
+		return;
+	}
+	////////////////////naredimo bufferje
+	
+	for (var i = 0; i < modelArray.length; i++) {
+		var susanVertices = modelArray[i].meshes[0].vertices;
+		var susanIndices = [].concat.apply([], modelArray[i].meshes[0].faces);
+		var susanTexCoords = modelArray[i].meshes[0].texturecoords[0];
+	
+		var susanPosVertexBufferObject = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, susanPosVertexBufferObject);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(susanVertices), gl.STATIC_DRAW);
+	
+		var susanTexCoordVertexBufferObject = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, susanTexCoordVertexBufferObject);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(susanTexCoords), gl.STATIC_DRAW);
+	
+		var susanIndexBufferObject = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, susanIndexBufferObject);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(susanIndices), gl.STATIC_DRAW);
+	
+		gl.bindBuffer(gl.ARRAY_BUFFER, susanPosVertexBufferObject);
+		var positionAttribLocation = gl.getAttribLocation(shaderProgram, 'vertPosition');
+		gl.vertexAttribPointer(
+			positionAttribLocation, // Attribute location
+			3, // Number of elements per attribute
+			gl.FLOAT, // Type of elements
+			gl.FALSE,
+			3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+			0 // Offset from the beginning of a single vertex to this attribute
+		);
+		gl.enableVertexAttribArray(positionAttribLocation);
+	
+		gl.bindBuffer(gl.ARRAY_BUFFER, susanTexCoordVertexBufferObject);
+		var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
+		gl.vertexAttribPointer(
+			texCoordAttribLocation, // Attribute location
+			2, // Number of elements per attribute
+			gl.FLOAT, // Type of elements
+			gl.FALSE,
+			2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+			0
+		);
+		gl.enableVertexAttribArray(texCoordAttribLocation);
+	}
+	
+	for(var i = 0; i < textureArray.length; i++) {
+		var susanTexture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, susanTexture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		gl.texImage2D(
+			gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+			gl.UNSIGNED_BYTE,
+			textureArray[i]
+		);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	} 
+	
+	// Tell OpenGL state machine which program should be active.
+	gl.useProgram(shaderProgram);
+
+	var matWorldUniformLocation = gl.getUniformLocation(shaderProgram, 'mWorld');
+	var matViewUniformLocation = gl.getUniformLocation(shaderProgram, 'mView');
+	var matProjUniformLocation = gl.getUniformLocation(shaderProgram, 'mProj');
+
+	var worldMatrix = new Float32Array(16);
+	var viewMatrix = new Float32Array(16);
+	var projMatrix = new Float32Array(16);
+	mat4.identity(worldMatrix);
+	mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+	mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
+
+	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+	var xRotationMatrix = new Float32Array(16);
+	var yRotationMatrix = new Float32Array(16);
+	
+	
+	
+	
+}
+
+//*
 var RunDemo = function (vertexShaderText, fragmentShaderText, SusanImage, SusanModel) {
 	console.log('This is working');
 
-	var canvas = document.getElementById('game-surface');
+	canvas = document.getElementById('game-surface');
 	gl = canvas.getContext('webgl');
 
 	if (!gl) {
@@ -57,8 +364,8 @@ var RunDemo = function (vertexShaderText, fragmentShaderText, SusanImage, SusanM
 	//
 	// Create shaders
 	// 
-	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 
 	gl.shaderSource(vertexShader, vertexShaderText);
 	gl.shaderSource(fragmentShader, fragmentShaderText);
@@ -195,3 +502,4 @@ var RunDemo = function (vertexShaderText, fragmentShaderText, SusanImage, SusanM
 	};
 	requestAnimationFrame(loop);
 };
+//*/
